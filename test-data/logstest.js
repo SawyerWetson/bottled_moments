5fdafd83d75d2e3a084df86ca1043021d4000657
TheMagicHatCoder <sawyer.wetson@gmail.com>
2025-11-26 14:11:14 -0600

    Add console logs for testing and debugging

---
+++ test-data/logstest.js
@@ -0,0 +1,13 @@
+
+console.log("Testing... Testing... 123");
+for(let i = 0; i < 1; i++) {
+  for(let j = 0; j < 1; j++) {
+    console.log("$PATH = $https://bottled-moments.com/");
+    console.log("subdomains:redirects - 'https://https-bottled-moments.com.onrender.com', 'https-bottled-moments-web.onrender.com'");
+    console.log("Live-data-feed: on");
+    console.log("Who are you and why are you reading the console?");
+    console.log("testing math...")
+    console.log("Square root of 5" + Math.sqrt(5));
+
+  }
+}
